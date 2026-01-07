bf91028b5bbc0d94e2aeeebc2fbdebb8242b4497
Harsh Shet <78957080+harshshet@users.noreply.github.com>
2026-01-07 23:18:50 +0530

    Add mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants.js";
+import connectDB from "./db/index.js";
+// require ('dotenv').config({path:"./env"})  the standard which is given on the website
+import dotenv from "dotenv";
+
+dotenv.config({path:"./env"})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+first approch
+import express from "express";
+const app = express()
+
+;(async() =>{
+    try {
+        await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}')
+        app.on("error",(error)=>{
+            console.log(error);
+            throw error;
+            
+        })   
+        
+        app.listen(process.env.PORT,()=>{
+            console.log('App running on port ${process.env.PORT}')
+        } )
+    } catch (error) {
+        console.log(error);
+    
+    }
+})()*/
