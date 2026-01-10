36d0e90751ef7af9d50c44e9d115a0b5745d3bac
Harsh Shet <78957080+harshshet@users.noreply.github.com>
2026-01-10 22:15:26 +0530

    Add Async Handler , Refresh token ,user model and Video Model

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -2,16 +2,16 @@ class ApiError extends Error{
     constructor(
         statusCode,
         message="Something went wrong",
-        error =[],
+        errors =[],
         statck=""
 
     ){
-        super();
+        super(message);
         this.statusCode = statusCode;
         this.message = message;
         this.data= null,
         this.success= false,
-        this.errors = this.errors
+        this.errors = errors
         if (statck) {
             this.statck= statck;
         }else{
--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -26,4 +26,6 @@ const asyncHandler = (fn) => (req, res, next) =>{
 
 
 
+
 export {asyncHandler}
+
